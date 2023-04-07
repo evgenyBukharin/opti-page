591b7e9add7ee7a9433398266a84e41a449afc45
Evgeniy <87270911+evgenyBukharin@users.noreply.github.com>
2023-04-07 11:27:42 +0500

    hidden form fix

--- src/js/components/order-form.js
+++ src/js/components/order-form.js
@@ -10,17 +10,9 @@ const header = document.querySelector(".header");
 import { disableScroll } from "../functions/disable-scroll";
 import { enableScroll } from "../functions/enable-scroll";
 
-function headerScrollEnable() {
-	header.style.width = "100%";
-	header.style.position = "fixed";
-	header.nextElementSibling.style.marginTop = header.offsetHeight + "px";
-}
+function headerScrollEnable() {}
 
-function headerScrollDisable() {
-	header.style.width = "unset";
-	header.style.position = "relative";
-	header.nextElementSibling.style.marginTop = 0;
-}
+function headerScrollDisable() {}
 
 if (openBtn !== null) {
 	openBtn.addEventListener("click", () => {
